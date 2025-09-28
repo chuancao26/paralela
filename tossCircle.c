78f70cdc6a659b10af127caf70a5acb56d9784a5
chuancao26 <chuancao@unsa.edu.pe>
2025-09-28 12:26:48 -0500

    firsst circle

---
+++ tossCircle.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <time.h>
+
+int main(int argc, char* argv[]) {
+    int rank, size;
+    long long int tosses, local_tosses;
+    long long int local_in_circle = 0;
+    long long int total_in_circle = 0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Solo el proceso 0 pide el número de lanzamientos
+    if (rank == 0) {
+        printf("Ingrese el numero total de lanzamientos: ");
+        fflush(stdout);
+        scanf("%lld", &tosses);
+    }
+
+    // Se comparte el total con todos
+    MPI_Bcast(&tosses, 1, MPI_LONG_LONG_INT, 0, MPI_COMM_WORLD);
+
+    // Cada proceso hace su parte
+    local_tosses = tosses / size;
+
+    // Inicializar generador de números aleatorios distinto en cada proceso
+    unsigned int seed = (unsigned int)(time(NULL) + rank);
+
+    for (long long int i = 0; i < local_tosses; i++) {
+        double x = (double)rand_r(&seed) / RAND_MAX * 2.0 - 1.0;
+        double y = (double)rand_r(&seed) / RAND_MAX * 2.0 - 1.0;
+        double distance_squared = x*x + y*y;
+        if (distance_squared <= 1.0) local_in_circle++;
+    }
+
+    // Reducimos sumando todos los locales
+    MPI_Reduce(&local_in_circle, &total_in_circle, 1, MPI_LONG_LONG_INT,
+               MPI_SUM, 0, MPI_COMM_WORLD);
+
+    // El proceso 0 calcula e imprime pi
+    if (rank == 0) {
+        double pi_estimate = 4.0 * (double)total_in_circle / ((double)tosses);
+        printf("Estimacion de pi = %.10f\n", pi_estimate);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
+
